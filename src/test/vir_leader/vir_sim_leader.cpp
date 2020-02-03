95ed56e58918e56bd060f9622e8cfc7002a3f381
lee-shun <2015097272@qq.com>
2020-02-03 15:52:19 +0800

    完成仿真领机

--- src/test/vir_leader/vir_sim_leader.cpp
+++ src/test/vir_leader/vir_sim_leader.cpp
@@ -1,3 +1,54 @@
 /**
  * 本程序是虚拟领机，领机获取的是仿真环境的数据，速度以及航线不是固定不变的
+ * 订阅打包程序的飞机状态，打包成虚拟领机的状态发布。
 */
+#include "vir_sim_leader.hpp"
+
+void VIR_SIM_LEADER::fw_state_cb(const fixed_wing_formation_control::FWstates::ConstPtr &msg)
+{
+    fwstates = *msg;
+}
+
+void VIR_SIM_LEADER::ros_sub_pub()
+{
+    vir_leader_pub = nh.advertise<fixed_wing_formation_control::Leaderstates>("fixed_wing_formation_control/leader_states", 10);
+    fw_states_sub = nh.subscribe<fixed_wing_formation_control::FWstates>("fixed_wing_formation_control/fw_states", 10, &VIR_SIM_LEADER::fw_state_cb, this);
+}
+
+void VIR_SIM_LEADER::run(int argc, char **argv)
+{
+    ros::Rate rate(10.0);
+    begin_time = get_sys_time(); // 记录启控时间
+    ros_sub_pub();               //继承而来，只是发布消息
+
+    while (ros::ok())
+    {
+
+        leaderstates.airspeed = fwstates.air_speed;
+        leaderstates.altitude = fwstates.altitude;
+        leaderstates.latitude = fwstates.latitude;
+        leaderstates.longtitude = fwstates.longtitude;
+        leaderstates.ned_vel_x = fwstates.ned_vel_x;
+        leaderstates.ned_vel_y = fwstates.ned_vel_y;
+        leaderstates.ned_vel_z = fwstates.ned_vel_z;
+
+        vir_leader_pub.publish(leaderstates);
+
+        ros::spinOnce(); //挂起一段时间，保证周期的速度
+        rate.sleep();
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "vir_dir_leader");
+
+    VIR_SIM_LEADER _vir_leader;
+
+    if (true)
+    {
+        _vir_leader.run(argc, argv);
+    }
+
+    return 0;
+}
--- src/test/vir_leader/vir_sim_leader.hpp
+++ src/test/vir_leader/vir_sim_leader.hpp
@@ -1,3 +1,33 @@
 /**
  * 本程序是虚拟领机，领机获取的是仿真环境的数据，速度以及航线不是固定不变的
+ * 订阅打包程序的飞机状态，打包成虚拟领机的状态发布。
 */
+#ifndef VIR_SIM_LEADER_HPP
+#define VIR_SIM_LEADER_HPP
+
+#include <ros/ros.h>
+#include <fixed_wing_formation_control/FWstates.h>
+#include <fixed_wing_formation_control/Leaderstates.h>
+#include "../../fixed_wing_lib/syslib.hpp"
+class VIR_SIM_LEADER
+{
+public:
+    void run(int argc, char **argv);
+
+private:
+    ros::NodeHandle nh;
+    ros::Publisher vir_leader_pub;
+    ros::Subscriber fw_states_sub;                           // 【订阅】固定翼全部状态量
+    fixed_wing_formation_control::Leaderstates leaderstates; //即将要发布的领机的状态
+    fixed_wing_formation_control::FWstates fwstates;         //自定义--飞机打包的全部状态
+
+    void ros_sub_pub();
+    void show_vir_leader_status();
+    void fw_state_cb(const fixed_wing_formation_control::FWstates::ConstPtr &msg);
+
+    long begin_time;
+    float current_time;
+    float last_time;
+};
+
+#endif
