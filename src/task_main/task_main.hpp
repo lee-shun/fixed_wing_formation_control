d96dab121502357df6a853953497930e8ada132e
lee-shun <2015097272@qq.com>
2020-02-09 19:45:55 +0800

    建立监控节点的输入以及输出

--- src/task_main/task_main.hpp
+++ src/task_main/task_main.hpp
@@ -8,42 +8,48 @@
 #include "fixed_wing_formation_control/FWcmd.h"
 #include "fixed_wing_formation_control/Leaderstates.h"
 #include "fixed_wing_formation_control/Formation_control_states.h"
+#include "fixed_wing_formation_control/Fwmonitor.h"
 #include "../formation_controller/formation_controller.hpp"
-#include "../pack_fw_states/fixed_wing_sub_pub.hpp"
 
 using namespace std;
 class TASK_MAIN
 {
 private:
+    int planeID{2}; //飞机编号
+
     ros::NodeHandle nh; //ros句柄
     ros::Time begin_time;
     float current_time;
     float get_ros_time(ros::Time begin); //获取当前时间
 
-    int planeID{2};                                                                         //飞机编号
-    _FIXED_WING_SUB_PUB fixed_wing_sub_pub;                                                 //ros消息收发的中间对象
-    fixed_wing_formation_control::FWstates fwstates;                                        //自定义--飞机打包的全部状态
-    fixed_wing_formation_control::FWcmd fw_4cmd;                                            //自定义--飞机四通道控制量
-    fixed_wing_formation_control::Leaderstates leaderstates;                                //自定义--领机状态
-    fixed_wing_formation_control::Formation_control_states formation_control_states;        //自定义--编队控制状态量
-    ros::Subscriber fw_states_sub;                                                          // 【订阅】固定翼全部状态量
-    ros::Subscriber leader_states_sub;                                                      // 【订阅】领机全部状态量
-    ros::Publisher formation_control_states_pub;                                            // 【发布】编队控制状态量
-    ros::Publisher fw_cmd_pub;                                                              //【发布】飞机四通道控制量
-    void fw_state_cb(const fixed_wing_formation_control::FWstates::ConstPtr &msg);          //
-    void leader_states_cb(const fixed_wing_formation_control::Leaderstates::ConstPtr &msg); //
-    void ros_sub_pub();                                                                     //订阅领机从机的信息
+    ros::Subscriber fwmonitor_sub;               // 【订阅】任务状态的flags
+    ros::Subscriber fw_states_sub;               // 【订阅】固定翼全部状态量
+    ros::Subscriber leader_states_sub;           // 【订阅】领机全部状态量
+    ros::Publisher formation_control_states_pub; // 【发布】编队控制状态量
+    ros::Publisher fw_cmd_pub;                   // 【发布】飞机四通道控制量
+
+    fixed_wing_formation_control::FWstates fwstates;                                 //自定义--飞机打包的全部状态
+    fixed_wing_formation_control::FWcmd fw_4cmd;                                     //自定义--飞机四通道控制量
+    fixed_wing_formation_control::Leaderstates leaderstates;                         //自定义--领机状态
+    fixed_wing_formation_control::Formation_control_states formation_control_states; //自定义--编队控制状态量
+    fixed_wing_formation_control::Fwmonitor fwmonitor_flag;                          //自定义--任务状态的flags
+
+    void ros_sub_pub();                                                                     //ros消息订阅与发布
+    void fw_fwmonitor_cb(const fixed_wing_formation_control::Fwmonitor::ConstPtr &msg);     //任务状态flagscallback
+    void fw_state_cb(const fixed_wing_formation_control::FWstates::ConstPtr &msg);          //本机状态callback
+    void leader_states_cb(const fixed_wing_formation_control::Leaderstates::ConstPtr &msg); //领机状态callback
 
     bool fw_is_ok{true};  //飞机失联、失控以及炸鸡标志位
     bool need_task{true}; //需要进行任务标志位
 
     bool need_take_off{false}; //起飞标志位
 
-    void control_formation();                                       //编队控制主函数
-    bool need_control_formation{true};                              //需要编队控制的标志位
-    FORMATION_CONTROL formation_controller;                         //编队控制器
-    string fw_col_mode_current{"MANUAL"};                           //当前模式
-    string fw_col_mode_last{"MANUAL"};                              //上一时刻模式
+    void control_formation();               //编队控制主函数
+    bool need_control_formation{true};      //需要编队控制的标志位
+    FORMATION_CONTROL formation_controller; //编队控制器
+    string fw_col_mode_current{"MANUAL"};   //当前模式
+    string fw_col_mode_last{"MANUAL"};      //上一时刻模式
+
     struct FORMATION_CONTROL::_s_leader_states leader_states;       //领机信息
     struct FORMATION_CONTROL::_s_fw_states thisfw_states;           //本机信息
     struct FORMATION_CONTROL::_s_4cmd formation_cmd;                //四通道控制量
