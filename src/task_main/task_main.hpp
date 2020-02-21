d208ced90ad0e212b5f03e2cf06720266e0bf398
lee-shun <2015097272@qq.com>
2020-02-21 16:13:51 +0800

    添加函数的注释，完成部分ros消息的输入输出构建

--- src/task_main/task_main.hpp
+++ src/task_main/task_main.hpp
@@ -8,7 +8,7 @@
  * @------------------------------------------2: 2------------------------------------------@
  * @LastEditors: lee-shun
  * @LastEditors_Email: 2015097272@qq.com
- * @LastEditTime: 2020-02-18 19:45:48
+ * @LastEditTime: 2020-02-21 12:01:49
  * @LastEditors_Organization: BIT-CGNC, fixed_wing_group
  * @LastEditors_Description:  
  * @------------------------------------------3: 3------------------------------------------@
@@ -25,6 +25,8 @@
 #include "fixed_wing_formation_control/Leaderstates.h"
 #include "fixed_wing_formation_control/Formation_control_states.h"
 #include "fixed_wing_formation_control/Fwmonitor.h"
+#include "fixed_wing_formation_control/Fw_cmd_mode.h"
+#include "fixed_wing_formation_control/Fw_current_mode.h"
 #include "../formation_controller/formation_controller.hpp"
 
 using namespace std;
@@ -39,30 +41,29 @@ private:
     float current_time;
     float get_ros_time(ros::Time begin); //获取当前时间
 
-    ros::Subscriber fwmonitor_sub;               // 【订阅】任务状态的flags
-    ros::Subscriber fw_states_sub;               // 【订阅】固定翼全部状态量
-    ros::Subscriber leader_states_sub;           // 【订阅】领机全部状态量
-    ros::Publisher formation_control_states_pub; // 【发布】编队控制状态量
-    ros::Publisher fw_cmd_pub;                   // 【发布】飞机四通道控制量
+    ros::Subscriber fwmonitor_sub;               //【订阅】来自fw_monitor任务状态的flags
+    ros::Subscriber fw_states_sub;               //【订阅】来自pack_fw_states固定翼全部状态量
+    ros::Subscriber leader_states_sub;           //【订阅】来自通讯节点，或者视觉节点领机全部状态量
+    ros::Subscriber fw_cmd_mode_sub;             //【订阅】来自commander状态机的控制指令
+    ros::Publisher formation_control_states_pub; //【发布】编队控制状态量
+    ros::Publisher fw_cmd_pub;                   //【发布】飞机四通道控制量
+    ros::Publisher fw_current_mode_pub;          //【发布】飞机当前处于的任务状态
 
     fixed_wing_formation_control::FWstates fwstates;                                 //自定义--飞机打包的全部状态
     fixed_wing_formation_control::FWcmd fw_4cmd;                                     //自定义--飞机四通道控制量
     fixed_wing_formation_control::Leaderstates leaderstates;                         //自定义--领机状态
     fixed_wing_formation_control::Formation_control_states formation_control_states; //自定义--编队控制状态量
     fixed_wing_formation_control::Fwmonitor fwmonitor_flag;                          //自定义--任务状态的flags
+    fixed_wing_formation_control::Fw_cmd_mode fw_cmd_mode;                           //自定义--来自commander的命令飞机模式
+    fixed_wing_formation_control::Fw_current_mode fw_current_mode;                   //自定义--要发布的飞机当前任务模式
 
     void ros_sub_pub();                                                                     //ros消息订阅与发布
     void fw_fwmonitor_cb(const fixed_wing_formation_control::Fwmonitor::ConstPtr &msg);     //任务状态flagscallback
     void fw_state_cb(const fixed_wing_formation_control::FWstates::ConstPtr &msg);          //本机状态callback
     void leader_states_cb(const fixed_wing_formation_control::Leaderstates::ConstPtr &msg); //领机状态callback
-
-    bool fw_is_ok{true};  //飞机失联、失控以及炸鸡标志位
-    bool need_task{true}; //需要进行任务标志位
-
-    bool need_take_off{false}; //起飞标志位
+    void fw_cmd_mode_cb(const fixed_wing_formation_control::Fw_cmd_mode::ConstPtr &msg);    //commander指令callback
 
     void control_formation();               //编队控制主函数
-    bool need_control_formation{true};      //需要编队控制的标志位
     FORMATION_CONTROL formation_controller; //编队控制器
     string fw_col_mode_current{"MANUAL"};   //当前模式
     string fw_col_mode_last{"MANUAL"};      //上一时刻模式
@@ -80,12 +81,6 @@ private:
     struct FORMATION_CONTROL::_s_formation_params formation_params; //编队控制器混合误差产生参数,编队控制器参数
     struct FORMATION_CONTROL::_s_fw_sp formation_sp;                //编队控制运动学期望值
     void formation_states_pub();                                    //发布编队控制器控制状态
-
-    bool need_landing{false}; //需要降落标志位
-
-    enum _e_TASK_STAGE //飞机任务状态阶段
-    {
-    };
 
 public:
     void run();
