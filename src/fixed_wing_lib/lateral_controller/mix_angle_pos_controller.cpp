33a3f7a44cc882efc7a04dd7e1b7052104353d47
lee-shun <lee970802@163.com>
2020-04-15 10:50:53 +0800

    初步完成所有控制器的编写

--- src/fixed_wing_lib/lateral_controller/mix_angle_pos_controller.cpp
+++ src/fixed_wing_lib/lateral_controller/mix_angle_pos_controller.cpp
@@ -13,3 +13,25 @@
  * @LastEditors_Description:  
  * @------------------------------------------3: 3------------------------------------------@
  */
+#include "mix_angle_pos_controller.hpp"
+
+void MIX_ANGLE_POS_CONTROLLER::update_angle_pos_err(float angle_err,
+                                                    float pos_err) {
+  angle_error = angle_err;
+
+  pos_error = pos_err;
+}
+void MIX_ANGLE_POS_CONTROLLER::set_mix_params(float kp_p, float kangle_p) {
+  mix_params.kp_p = kp_p;
+  mix_params.kangle_p = kangle_p;
+}
+void MIX_ANGLE_POS_CONTROLLER::set_pid_params(float mix_kp, float mix_ki,
+                                             float mix_kd) {
+  mix_params.mix_kp = mix_kp;
+  mix_params.mix_ki = mix_ki;
+  mix_params.mix_kd = mix_kd;
+}
+float MIX_ANGLE_POS_CONTROLLER::mix_angle_pos_controller() {
+  cout << "in the mix_angle_pos_controller!!!!" << endl;
+  return 0.0;
+}
