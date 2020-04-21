0bab9a2dfb1c304230aa7e8eac3df2321faf236c
lee-shun <lee970802@163.com>
2020-04-22 00:22:11 +0800

    还差模式切换的仿真用主程序

--- src/pack_fw_states/pack_fw_states.cpp
+++ src/pack_fw_states/pack_fw_states.cpp
@@ -19,101 +19,111 @@
 
 #include "pack_fw_states.hpp"
 
+void PACK_FW_STATES::set_planeID(int id) {
+  switch (id) {
+  case 1: uavID = "uav1/";
+    break;
+  case 2: uavID = "uav2/";
+    break;
+  case 3: uavID = "uav3/";
+    break;
+  }
+}
 void PACK_FW_STATES::ros_sub_and_pub() {
   //##########################################订阅mavros消息+控制器指令cmd###################################################//
   fixed_wing_states_sub                  //
       = nh.subscribe<mavros_msgs::State> //
-        (add2str(planeID, "mavros/state"), 10, &_FIXED_WING_SUB_PUB::state_cb,
+        (add2str(uavID, "mavros/state"), 10, &_FIXED_WING_SUB_PUB::state_cb,
          &fixed_wing_sub_pub);
 
   // 【订阅】无人机imu信息，
   fixed_wing_imu_sub                   //
       = nh.subscribe<sensor_msgs::Imu> //
-        (add2str(planeID, "mavros/imu/data"), 10, &_FIXED_WING_SUB_PUB::imu_cb,
+        (add2str(uavID, "mavros/imu/data"), 10, &_FIXED_WING_SUB_PUB::imu_cb,
          &fixed_wing_sub_pub);
 
   // 【订阅】无人机gps位置
   fixed_wing_global_position_form_px4_sub    //
       = nh.subscribe<sensor_msgs::NavSatFix> //
-        (add2str(planeID, "mavros/global_position/global"), 10,
+        (add2str(uavID, "mavros/global_position/global"), 10,
          &_FIXED_WING_SUB_PUB::global_position_form_px4_cb,
          &fixed_wing_sub_pub);
 
   //【订阅】无人机gps相对alt
   fixed_wing_global_rel_alt_from_px4_sub //
       = nh.subscribe<std_msgs::Float64>  //
-        (add2str(planeID, "mavros/global_position/rel_alt"), 10,
+        (add2str(uavID, "mavros/global_position/rel_alt"), 10,
          &_FIXED_WING_SUB_PUB::fixed_wing_global_rel_alt_from_px4_cb,
          &fixed_wing_sub_pub);
 
   // 【订阅】无人机ump位置
   fixed_wing_umt_position_from_px4_sub   //
       = nh.subscribe<nav_msgs::Odometry> //
-        (add2str(planeID, "mavros/global_position/local"), 10,
+        (add2str(uavID, "mavros/global_position/local"), 10,
          &_FIXED_WING_SUB_PUB::umt_position_from_px4_cb, &fixed_wing_sub_pub);
 
   // 【订阅】无人机gps三向速度
   fixed_wing_velocity_global_fused_from_px4_sub   //
       = nh.subscribe<geometry_msgs::TwistStamped> //
-        (add2str(planeID, "mavros/global_position/raw/gps_vel"), 10,
+        (add2str(uavID, "mavros/global_position/raw/gps_vel"), 10,
          &_FIXED_WING_SUB_PUB::velocity_global_fused_from_px4_cb,
          &fixed_wing_sub_pub);
 
   // 【订阅】无人机ned位置
   fixed_wing_local_position_from_px4             //
       = nh.subscribe<geometry_msgs::PoseStamped> //
-        (add2str(planeID, "mavros/local_position/pose"), 10,
+        (add2str(uavID, "mavros/local_position/pose"), 10,
          &_FIXED_WING_SUB_PUB::local_position_from_px4_cb, &fixed_wing_sub_pub);
 
   // 【订阅】无人机ned三向速度
   fixed_wing_velocity_ned_fused_from_px4_sub      //
       = nh.subscribe<geometry_msgs::TwistStamped> //
-        (add2str(planeID, "mavros/local_position/velocity_local"), 10,
+        (add2str(uavID, "mavros/local_position/velocity_local"), 10,
          &_FIXED_WING_SUB_PUB::velocity_ned_fused_from_px4_cb,
          &fixed_wing_sub_pub);
 
   // 【订阅】无人机ned三向加速度
   fixed_wing_acc_ned_from_px4_sub                               //
       = nh.subscribe<geometry_msgs::AccelWithCovarianceStamped> //
-        (add2str(planeID, "mavros/local_position/accel"), 10,
+        (add2str(uavID, "mavros/local_position/accel"), 10,
          &_FIXED_WING_SUB_PUB::acc_ned_from_px4_cb, &fixed_wing_sub_pub);
 
   // 【订阅】无人机ned三向加速度
   fixed_wing_wind_estimate_from_px4_sub                         //
       = nh.subscribe<geometry_msgs::TwistWithCovarianceStamped> //
-        (add2str(planeID, "mavros/wind_estimation"), 10,
+        (add2str(uavID, "mavros/wind_estimation"), 10,
          &_FIXED_WING_SUB_PUB::wind_estimate_from_px4_cb, &fixed_wing_sub_pub);
 
   fixed_wing_battrey_state_from_px4_sub         //
       = nh.subscribe<sensor_msgs::BatteryState> //
-        (add2str(planeID, "mavros/battery"), 10,
+        (add2str(uavID, "mavros/battery"), 10,
          &_FIXED_WING_SUB_PUB::battrey_state_from_px4_cb, &fixed_wing_sub_pub);
 
   fixed_wing_waypoints_sub //// 【订阅】无人机当前航点
       = nh.subscribe<mavros_msgs::WaypointList> //
-        (add2str(planeID, "mavros/mission/waypoints"), 10,
+        (add2str(uavID, "mavros/mission/waypoints"), 10,
          &_FIXED_WING_SUB_PUB::waypointlist_from_px4_cb, &fixed_wing_sub_pub);
 
   fixed_wing_waypointsreach_sub //// 【订阅】无人机到达的航点
       = nh.subscribe<mavros_msgs::WaypointReached> //
-        (add2str(planeID, "mavros/mission/reached"), 10,
+        (add2str(uavID, "mavros/mission/reached"), 10,
          &_FIXED_WING_SUB_PUB::waypoints_reached_from_px4_cb,
          &fixed_wing_sub_pub);
 
   fixed_wing_altitude_from_px4_sub          //订阅高度
       = nh.subscribe<mavros_msgs::Altitude> //
-        (add2str(planeID, "mavros/altitude"), 10,
+        (add2str(uavID, "mavros/altitude"), 10,
          &_FIXED_WING_SUB_PUB::altitude_from_px4_cb, &fixed_wing_sub_pub);
 
   fixed_wing_air_ground_speed_from_px4_sub //订阅空速、地速
       = nh.subscribe<mavros_msgs::VFR_HUD> //
-        (add2str(planeID, "mavros/vfr_hud"), 10,
+        (add2str(uavID, "mavros/vfr_hud"), 10,
          &_FIXED_WING_SUB_PUB::air_ground_speed_from_px4_cb,
          &fixed_wing_sub_pub);
 
   fixed_wing_cmd_from_controller_sub //订阅来自上层控制器的四通道控制量
       = nh.subscribe<fixed_wing_formation_control::FWcmd> //
-        ("/fixed_wing_formation_control/fw_cmd", 10,
+        (add2str(uavID,"fixed_wing_formation_control/fw_cmd"), 10,
          &_FIXED_WING_SUB_PUB::cmd_from_controller_cb, &fixed_wing_sub_pub);
 
   //##########################################订阅mavros消息+控制器指令cmd###################################################//
@@ -124,17 +134,17 @@ void PACK_FW_STATES::ros_sub_and_pub() {
   //##########################################发布mavros消息+飞机状态states###################################################//
 
   fixed_wing_local_pos_sp_pub = nh.advertise<mavros_msgs::PositionTarget>(
-      add2str(planeID, "mavros/setpoint_raw/local"), 10);
+      add2str(uavID, "mavros/setpoint_raw/local"), 10);
 
   fixed_wing_global_pos_sp_pub =
       nh.advertise<mavros_msgs::GlobalPositionTarget>(
-          add2str(planeID, "mavros/setpoint_raw/global"), 10);
+          add2str(uavID, "mavros/setpoint_raw/global"), 10);
 
   fixed_wing_local_att_sp_pub = nh.advertise<mavros_msgs::AttitudeTarget>(
-      add2str(planeID, "mavros/setpoint_raw/attitude"), 10);
+      add2str(uavID, "mavros/setpoint_raw/attitude"), 10);
 
   fixed_wing_states_pub = nh.advertise<fixed_wing_formation_control::FWstates>(
-      "fixed_wing_formation_control/fw_states", 10);
+          add2str(uavID,"fixed_wing_formation_control/fw_states"), 10);
 
   //##########################################发布消息###################################################//
   //
@@ -144,23 +154,23 @@ void PACK_FW_STATES::ros_sub_and_pub() {
   //##########################################服务###################################################//
   // 服务 修改系统模式
   set_mode_client = nh.serviceClient<mavros_msgs::SetMode>(
-      add2str(planeID, "mavros/set_mode"));
+      add2str(uavID, "mavros/set_mode"));
 
   arming_client = nh.serviceClient<mavros_msgs::CommandBool>(
-      add2str(planeID, "mavros/cmd/arming"));
+      add2str(uavID, "mavros/cmd/arming"));
 
   waypoint_setcurrent_client =
       nh.serviceClient<mavros_msgs::WaypointSetCurrent>(
-          add2str(planeID, "mavros/mission/set_current"));
+          add2str(uavID, "mavros/mission/set_current"));
 
   waypoint_pull_client = nh.serviceClient<mavros_msgs::WaypointPull>(
-      add2str(planeID, "mavros/mission/pull"));
+      add2str(uavID, "mavros/mission/pull"));
 
   waypoint_push_client = nh.serviceClient<mavros_msgs::WaypointPush>(
-      add2str(planeID, "mavros/mission/push"));
+      add2str(uavID, "mavros/mission/push"));
 
   waypoint_clear_client = nh.serviceClient<mavros_msgs::WaypointClear>(
-      add2str(planeID, "mavros/mission/clear"));
+      add2str(uavID, "mavros/mission/clear"));
   //##########################################服务###################################################//
   //
 }
@@ -311,17 +321,19 @@ void PACK_FW_STATES::run(int argc, char **argv) {
   ros::Rate rate(50.0);
 
   ros_sub_and_pub();
-  cout << "打包中，，，请查收，，" << endl;
+  PACK_FW_STATES_INFO("打包开始");
 
   long begin_time = get_sys_time();
 
   while (ros::ok()) {
-    print_counter++;
-    if (print_counter >= 100) {
-      cout << "现在距离程序开始已经过了：" << fixed << setprecision(5)
-           << double(get_time_from_begin(begin_time)) / 1000 << " 秒"
-           << "仍在打包" << endl;
 
+    print_counter++;
+
+    if (print_counter >= 100) {
+      PACK_FW_STATES_INFO("本机ID:" << planeID);
+      PACK_FW_STATES_INFO(
+          "历时:" << fixed << setprecision(5)
+                  << double(get_time_from_begin(begin_time)) / 1000 << " 秒");
       print_counter = 0;
     }
 
@@ -333,12 +345,15 @@ void PACK_FW_STATES::run(int argc, char **argv) {
     ros::spinOnce(); //挂起一段时间，保证周期的速度
     rate.sleep();
   }
+  PACK_FW_STATES_INFO("打包退出");
 }
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "pack_fw_states");
 
   PACK_FW_STATES _pack;
   if (true) {
+    _pack.set_planeID(1);
     _pack.run(argc, argv);
   }
 
--- src/test/switch_fw_mode/switch_fw_mode.cpp
+++ src/test/switch_fw_mode/switch_fw_mode.cpp
@@ -15,48 +15,45 @@
  * @------------------------------------------3: 3------------------------------------------@
  */
 
-/*  ros程序必备头文件 */
-#include <iostream>
-#include <ros/ros.h>
-/* mavros相关头文件 */
-#include "fixed_wing_formation_control/Fw_cmd_mode.h"
-#include <mavros_msgs/SetMode.h>
-#include <mavros_msgs/State.h>
+#include "switch_fw_mode.hpp"
 
-using namespace std;
+/**
+ * @Input: int
+ * @Output:
+ * @Description: 设定当前飞机的ID
+ */
+void SWITCH_FW_MODE::set_planeID(int id) {
+  planeID = id;
+  switch (planeID) {
+  case 1:
+    uavID = "uav1/";
+    break;
+  case 2:
+    uavID = "uav2/";
+    break;
+  case 3:
+    uavID = "uav3/";
+    break;
+  }
+}
 
-mavros_msgs::State
-    current_state; /* 无人机当前状态[包含上锁状态 模式] (从飞控中读取) */
-float get_ros_time(ros::Time begin); /* 获取ros当前时间 */
+void SWITCH_FW_MODE::run(int argc, char **argv) {
 
-/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>回 调 函
- * 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
-
-void state_cb(const mavros_msgs::State::ConstPtr &msg) { current_state = *msg; }
-/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>主 函
- * 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
-int main(int argc, char **argv) {
   ros::init(argc, argv, "switch_fw_mode");
-  ros::NodeHandle nh;
-  ros::Rate rate(5.0);                     /*  频率 [5Hz] */
-  ros::Time begin_time = ros::Time::now(); /* 记录启控时间 */
-  mavros_msgs::SetMode mode_cmd;           /*  模式容器 */
-  fixed_wing_formation_control::Fw_cmd_mode task_cmd_mode; /* 任务模式切换 */
 
+  ros::Rate rate(5.0); /*  频率 [5Hz] */
+
+  /* 【订阅】无人机当前状态 */
   ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>(
-      "mavros/state", 10, state_cb); /* 【订阅】无人机当前状态 */
+      add2str(uavID, "mavros/state"), 10, &SWITCH_FW_MODE::state_cb, this);
 
   ros::Publisher task_cmd_mode_pub =
       nh.advertise<fixed_wing_formation_control::Fw_cmd_mode>(
-          "fixed_wing_formation_control/fw_cmd_mode", 10);
+          add2str(uavID, "fixed_wing_formation_control/fw_cmd_mode"), 10);
 
-  ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>(
-      "mavros/set_mode"); /* 【服务】 修改系统模式 */
-
-  int mode_type = 0;    /* 模式类型 */
-  int times_out = 100;  /* 次数限制 */
-  int counters = 0;     /* 计数器 */
-  bool outflag = false; /* 退出标志位 */
+  /* 【服务】 修改系统模式 */
+  ros::ServiceClient set_mode_client =
+      nh.serviceClient<mavros_msgs::SetMode>(add2str(uavID, "mavros/set_mode"));
 
   while (ros::ok() && (!outflag)) {
     float current_time = get_ros_time(begin_time); /* 当前时间 */
@@ -79,13 +76,13 @@ int main(int argc, char **argv) {
       outflag = true;
       break;
     }
+    /*  如果当前模式与设定模式不一致，切换，（有次数限制） */
+    while ((mode_cmd.request.custom_mode != current_state.mode) &&
+           (counters <= times_out)) {
 
-    while (
-        (mode_cmd.request.custom_mode != current_state.mode) &&
-        (counters <=
-         times_out)) { /*  如果当前模式与设定模式不一致，切换，（有次数限制） */
       counters++;
       set_mode_client.call(mode_cmd);
+
     }
 
     ros::spinOnce(); /*  挂起一段时间，等待切换结果 */
@@ -93,10 +90,9 @@ int main(int argc, char **argv) {
 
     counters = 0;
   }
-  return 0;
 }
 
-float get_ros_time(ros::Time begin) {
+float SWITCH_FW_MODE::get_ros_time(ros::Time begin) {
   ros::Time time_now = ros::Time::now();
   float currTimeSec = time_now.sec - begin.sec;
   float currTimenSec = time_now.nsec / 1e9 - begin.nsec / 1e9;
